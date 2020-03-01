2827123a69ab540cb60665737cf8ab0cc0c8401c
Hasan Abdel Halim <habdel@amazon.com>
2020-03-01 21:14:19 +0200

    regenerate prod dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBsvnyjHWxwEk5hYIHcdMaTL5u0trm3ZM4',
+    authDomain: 'whats-for-lunch-f4956.firebaseapp.com',
+    databaseURL: 'https://whats-for-lunch-f4956.firebaseio.com',
+    projectId: 'whats-for-lunch-f4956',
+    storageBucket: 'whats-for-lunch-f4956.appspot.com',
+    messagingSenderId: '83822380105',
+    appId: '1:83822380105:web:aa692ee562fafa41276b98'
+  }
 };
