b06d7213a67e44a782248ff710b46e30fed05220
Hasan Abdel Halim <habdel@amazon.com>
2020-03-05 23:49:54 +0200

    enforce security for today-selection and restaurant

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {RestaurantsComponent} from './component/restaurants/restaurants.component';
 import {OrderComponent} from './component/order/order.component';
 import {TodaySelectionComponent} from './component/today-selection/today-selection.component';
+import {AngularFireAuthGuard} from '@angular/fire/auth-guard';
 
 
 const routes: Routes = [
-  {path: 'restaurants', component: RestaurantsComponent},
-  {path: 'today-selection', component: TodaySelectionComponent},
-  {path: '', component: OrderComponent}
+  {path: 'order-now', component: OrderComponent},
+  {path: 'restaurants', component: RestaurantsComponent, canActivate: [AngularFireAuthGuard]},
+  {path: 'today-selection', component: TodaySelectionComponent, canActivate: [AngularFireAuthGuard]},
+  {path: '', redirectTo: 'order-now', pathMatch: 'full'}
 ];
 
 @NgModule({
