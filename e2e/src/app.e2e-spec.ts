0cc8d21d1eb9318551ee28171b2b3242666fe323
Hasan Abdel Halim <habdel@amazon.com>
2020-03-07 21:27:58 +0200

    e2e now tests adding a meal

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,16 +1,32 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, Ptor, ExpectedConditions, Key } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
   it('should display welcome message', () => {
-    page.navigateTo();
     expect(page.getTitleText()).toEqual('What\'s for lunch');
+  });
+
+  it('should display nav bar', () => {
+    expect(page.getToolbar().getNavbar()).toContain('Home')
+  });
+
+  it('should display login button', () => {
+    expect(page.getToolbar().getUserInfo()).toContain('LOGIN');
+  });
+
+  it('should be able to add a meal', () => {
+    const meal = page.getOrderForm().getMealInput();
+    browser.wait(ExpectedConditions.presenceOf(meal), 10000);
+    meal.sendKeys('first meal', Key.ENTER);
+    const ordersList = page.getOrderForm().getOrdersList();
+    expect(ordersList.getText()).toContain('first meal');
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,37 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, WebElement, WebElementPromise, ElementFinder } from 'protractor';
 
 export class AppPage {
+  getOrderForm(): OrderForm {
+    return new OrderForm();
+  }
+  getToolbar(): Toolbar {
+    return new Toolbar();
+  }
   navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl) as Promise<unknown>;
   }
 
   getTitleText(): Promise<string> {
     return element(by.css('app-root app-toolbar #toolbar .row .column h2')).getText() as Promise<string>;
+  }
+}
+export class OrderForm {
+  getMealInput(): ElementFinder {
+    return element(by.id('meal'));
+  }
+
+  getOrdersList(): ElementFinder {
+    return element(by.id('ordersList'));
+  }
+
+}
+
+export class Toolbar {
+  getUserInfo(): Promise<string> {
+    return element(by.id('userInfo')).getText() as Promise<string>;
+  }
+
+  getNavbar(): Promise<string> {
+    return element(by.css('#toolbar nav')).getText() as Promise<string>;
   }
 }
