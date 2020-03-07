f19207c7e401ed3799947b6b18329a62e34029fb
Hasan Abdel Halim <habdel@amazon.com>
2020-03-07 19:16:05 +0200

    e2e working again but not complete yet

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('whats-for-lunch app is running!');
+    expect(page.getTitleText()).toEqual('What\'s for lunch');
   });
 
   afterEach(async () => {
