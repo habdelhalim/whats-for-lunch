941f1b858e226b8d37d315a78ea47eb2d4ea4c56
Hasan Abdel Halim <habdel@amazon.com>
2020-03-09 02:34:07 +0200

    calculate order summary and details in cart

--- src/app/component/cart/cart.component.ts
+++ src/app/component/cart/cart.component.ts
@@ -10,11 +10,30 @@ import { Order } from 'src/app/model/order';
 })
 export class CartComponent implements OnInit {
   orders: Observable<Order[]>;
+  summary: any = {};
+  total: number = 0;
+  amount: number = 0;
 
   constructor(private service: CartService) { }
 
   ngOnInit(): void {
     this.orders = this.service.getCart();
+    this.orders.subscribe(
+      val => val.forEach(order => {
+        this.total += Number(order.quantity)
+        if (this.summary[order.description]) {
+          this.summary[order.description].quantity = Number(this.summary[order.description].quantity) + Number(order.quantity)
+        } else {
+          this.summary[order.description] = { quantity: Number(order.quantity), price: 0 }
+        }
+      })
+    )
+  }
+
+  updateAmount() {
+    this.amount = 0;
+    Object.keys(this.summary)
+      .forEach(key => this.amount += Number(this.summary[key].price) * Number(this.summary[key].quantity))
   }
 
 }
